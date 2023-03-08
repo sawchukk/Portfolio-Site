f334432ee92458528c3dd3b457b3bd91a812ec2f
sawchukk <sawchukk@gmail.com>
2023-03-08 02:18:41 -0800

    project subsections

---
+++ src/components/projSubsection/ProjSubsection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './projSubsection.css';
+
+const ProjSubsection = () => {
+    return(
+    <div className="web_projSubsection">
+        <div className="web_projSubsection_content">
+            <div className="web_projSubsection_content-name">
+                <h2> project1 </h2>
+            </div>
+            <div className="web_projSubsection_content-text">
+                <p> type </p>
+            </div>
+        </div>
+        <hr></hr>
+     </div>
+     
+    
+    )
+}
+
+export default ProjSubsection
