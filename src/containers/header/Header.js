6ba6d6d65f650556132b8fbf3ddd748dacb2a4a5
sawchukk <sawchukk@gmail.com>
2023-03-08 02:17:17 -0800

    projects section formatting and animated down arrow

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -10,17 +10,20 @@ const Header = () => {
                 <h1 className="main_text-first"> Karalyn </h1>
                 <h1 className="main_text-last"> Sawchuk </h1>
 
-                <div className="web_header-content_next">
-                    <button type="button"> About Me </button>
+            <div className="web_header-content_next">
+                <div className="web_header-content_next-arrow">
+                    <svg width="51" height="113" viewBox="0 0 51 113" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path id="check" d="M4.60817 101.432L21.4516 109.512L29.2692 94.1457" stroke="white" strokeWidth="5"/>
+                        <path id="squiggle" d="M34.9137 2C34.9137 2 60.5641 51.9267 40.2436 53.9928C28.0337 55.2342 25.5676 38.8169 13.3402 37.7609C-13.8029 35.4167 21.0738 107 21.0738 107" stroke="white" strokeWidth="5"/>
+                    </svg>
                 </div>
+                <button type="button"> About Me </button>
+            </div>
 
             </div>
             <div className="web_header-image">
-                    <img src={me} alt="me smiling"/>
-                </div>
-
-
-            Header
+                <img src={me} alt="me smiling"/>
+            </div>
         </div>
     )
 }
