865c31be7a91f7f04dd4490a6b1bbf46c54e052a
sawchukk <sawchukk@gmail.com>
2023-03-07 17:02:45 -0800

    outline for project section, centered about text

--- src/containers/proj/Proj.js
+++ src/containers/proj/Proj.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './proj.css';
+import { ProjSubsection } from './../../components';
 import projPreview from './../../assets/projPreview.png'
 
 const Proj = () => {
@@ -8,25 +9,17 @@ const Proj = () => {
             <div className="web_proj-content">
                 <h1> Projects </h1>
                 <hr></hr>
-                <div className="web_proj-content_sub">
-                    <div className="web_proj-content_sub-name">
-                        <h2> project1 </h2>
-                    </div>
-                    <div className="web_proj-content_sub-text">
-                        <p> type </p>
-                    </div>
-                </div>
-
-
+                <ProjSubsection />
+                <ProjSubsection />
+                <ProjSubsection />
+                <ProjSubsection />
+                <p> "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."</p>
 
             </div>
             <div className="web_proj-image">
                 <img src={projPreview} alt="project preview"/>
 
             </div>
-
-
-
 
         </div>
     )
